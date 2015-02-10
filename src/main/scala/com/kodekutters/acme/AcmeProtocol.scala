6be9f443c050dc56d900ae5661907ba310121eee
workingDog <ringowathelet@gmail.com>
2015-02-10 09:56:46 +0900

    initial commit

--- src/main/scala/com/kodekutters/acme/AcmeProtocol.scala
+++ src/main/scala/com/kodekutters/acme/AcmeProtocol.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Ringo Wathelet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kodekutters.acme
 
 import com.nimbusds.jose.jwk.JWK
@@ -6,7 +22,7 @@ import play.api.libs.json._
 /**
  * ACME protocol objects and messages package
  *
- * Reference, the Let's Encrypt project at: https://letsencrypt.org/
+ * Reference: Let's Encrypt project at: https://letsencrypt.org/
  *
  * For the ACME protocol specification see:
  * https://github.com/letsencrypt/acme-spec and
